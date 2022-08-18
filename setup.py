40bed2fd92431722e87943fa339d97d513bf30f9
Johannes11833 <j.gundlach@me.com>
2022-08-18 21:09:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ setup(
     version='0.1',
     description='A python wrapper for rclone.',
     author='Johannes Gundlach',
-    author_email='johannesgundlach97@gmail.com',
     install_requires=['simple_term_menu', 'alive-progress', ],
     packages=['rclone_python'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords=['rclone', 'wrapper', 'cloud sync'],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
