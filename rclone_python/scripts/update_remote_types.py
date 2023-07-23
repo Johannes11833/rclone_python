cee99f58a04e6ef4c69102bf3d45eb0f5e77525d
Johannes11833 <j.gundlach@me.com>
2023-07-23 15:52:57 +0200

    Feature/update remotes (#14)
    Periodically update the remote_types.py file automatically.

--- rclone_python/scripts/update_remote_types.py
+++ rclone_python/scripts/update_remote_types.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+
+
 import json
 import subprocess as sp
+from rclone_python import rclone
 
 
 def extract_remote_names(output_path: str = None) -> str:
-    """Extracts all the remote types supported by rclone.
+    """Updates the remote_types.py file to the newest supported backends.
 
     Args:
         output_path (str, optional): If provided, save the remote names as a .txt file.
@@ -11,6 +15,8 @@ def extract_remote_names(output_path: str = None) -> str:
     Returns:
         str: String containing remote type names as "var_name = remote_name"
     """
+
+    # get all supported backends
     rclone_output = sp.check_output("rclone config providers", shell=True)
     data = json.loads(rclone_output)
 
@@ -25,14 +31,23 @@ def extract_remote_names(output_path: str = None) -> str:
 
         var_name = name.replace(" ", "_")
 
-        output += f'{var_name}="{name}"\n'
+        output += f'    {var_name} = "{name}"\n'
 
     if output_path:
         with open(output_path, "w") as o:
-            o.write(output)
+            o.write("from enum import Enum")
+            o.write("\n\n\nclass RemoteTypes(Enum):")
+            o.write(
+                f'\n    """These are all the cloud systems support by rclone (generated with {rclone.version()}).'
+            )
+            o.write(
+                "\n    A more detailed overview can be found here: https://rclone.org/overview/"
+            )
+            o.write('\n    """\n\n')
+            o.write(f"{output}")
 
     return output
 
 
 if __name__ == "__main__":
-    extract_remote_names(output_path="rclone_remote_names.txt")
+    extract_remote_names(output_path="rclone_python/remote_types.py")
