d9f00b1b287d0d2a37895992b9482b925aa4725e
Johannes11833 <j.gundlach@me.com>
2023-07-24 22:05:26 +0200

    Reset main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from rclone_python import rclone
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # rclone.create_remote('test1', remote_types.RemoteTypes.box)
     print(rclone.get_remotes())
-
-    # print(rclone.about("onedrive"))
-    # rclone.copy("data/abc", "onedrive:data_new/ac7")
-    # # rclone.copy("data/image.jpg", "onedrive:new_image.jpg")
-    # rclone.sync("test_bisync", "box:data_new")
-    # print(rclone.hash("sha1", "box:data_new"))
-    # print(rclone.hash("sha1", "box:data_new/video2.webm"))
-    # print(rclone.hash("sha1", "box:data_new", checkfile="checkfile"))
-    print(rclone.hash("sha1", "box:data_new/video1.webm", checkfile="checkfile_vid1"))
-
-    # print(f"#{rclone.version(check=True)}#")
-    # print(f"#{rclone.version(check=False)}#")
+    print(rclone.ls('onedrive:data', max_depth=1, files_only=True))
+    rclone.copy('data', 'onedrive:data_new')
+    rclone.copy('data/image.jpg', 'onedrive:new_image.jpg')
