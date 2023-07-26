3bf39d282d722a00bb60b7336d6d2d43203c7959
Johannes11833 <Johannes11833@users.noreply.github.com>
2023-07-26 15:56:57 +0000

    :art: Format Python code with psf/black

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ from setuptools import setup
 
 from rclone_python import VERSION
 
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='rclone-python',
+    name="rclone-python",
     version=VERSION,
-    description='A python wrapper for rclone.',
-    author='Johannes Gundlach',
-    url='https://github.com/Johannes11833/rclone_python',
-    install_requires=['rich'],
-    packages=['rclone_python'],
+    description="A python wrapper for rclone.",
+    author="Johannes Gundlach",
+    url="https://github.com/Johannes11833/rclone_python",
+    install_requires=["rich"],
+    packages=["rclone_python"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords=['rclone', 'wrapper', 'cloud sync'],
-    python_requires='>=3.6',
+    keywords=["rclone", "wrapper", "cloud sync"],
+    python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
