44ad7e6593c166473a83137c87049d85a79ab297
Johannes Gundlach <j.gundlach@me.com>
2024-11-29 17:33:33 +0100

    Fix test_mkdir

--- tests/test_mkdir.py
+++ tests/test_mkdir.py
@@ -3,7 +3,7 @@ from rclone_python import rclone
 
 def test_mkdir(tmp_remote_folder):
     folder_name = "dir1  v2"
-    rclone.mkdir(f'{tmp_remote_folder}/"{folder_name}"')
+    rclone.mkdir(f'{tmp_remote_folder}/{folder_name}')
 
     output = rclone.ls(tmp_remote_folder)
 
