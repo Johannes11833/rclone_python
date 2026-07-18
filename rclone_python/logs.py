27e3a8b586c181fb3ba9793457f9e65ecea3b854
Justus Hildebrand <justus-hildebrand@users.noreply.github.com>
2026-07-19 00:14:24 +0200

    fix logging to no longer write to root logger (#74)

--- rclone_python/logs.py
+++ rclone_python/logs.py
@@ -1,7 +1,12 @@
 import logging
 from rich.logging import RichHandler
 
+logger = logging.getLogger("rclone_python")
 
-FORMAT = "%(message)s"
-logging.basicConfig(format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
-logger = logging.getLogger(__name__)
+if not logger.handlers:
+    handler = RichHandler()
+    handler.setFormatter(logging.Formatter("%(message)s", datefmt="[%X]"))
+    logger.addHandler(handler)
+
+logger.setLevel(logging.INFO)
+logger.propagate = False
